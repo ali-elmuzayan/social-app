dda2b622c0cd720dc9f37b4906521802566e98a8
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-05-06 21:00:09 +0300

    backend: implement  cors configuration

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { globalErrorHandler } from "./middleware/errorMiddleware";
 
 export const createApp = () => {
@@ -9,6 +10,12 @@ export const createApp = () => {
   // Middleware setup
   app.use(express.json());
   app.use(cookieParser());
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true, // allow credentials (cookies)
+    }),
+  );
 
   // Routes
   app.use("/api/v1/auth", authRoutes);
