b510d984afc593a4478d987e884ba186f9ec6f62
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-05-06 20:22:36 +0300

    implement the frontned application with vite 8 and typescript, add the tailwindcss and configure, add the shadcn ui for the User Interface components

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="text-center mt-50 font-bold text-gray-900 text-4xl">
+      App
+      <Button>Click On me</Button>
+    </div>
+  );
+}
+
+export default App;
